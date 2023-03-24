0509eb76f9337a0b4f6726d1173c7937eaf185db
Oleg <olegashepelev@gmail.com>
2023-03-25 00:01:16 +0600

    Complete second task

--- task1.py
+++ task1.py
@@ -4,3 +4,16 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)
 
 print(fib(7))
+
+########################################
+
+num = int(input('Please input number: '))
+
+
+def fibonachy(n):
+    if n <= 1:
+        return n
+    return fibonachy(n - 1) + fibonachy(n - 2)
+
+
+print(fibonachy(num))
